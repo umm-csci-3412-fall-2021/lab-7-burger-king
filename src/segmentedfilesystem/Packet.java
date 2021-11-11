0340c65030d5695183d6fe29e34d45fbba172fd5
BananaObserved <golds349@morris.umn.edu>
2021-11-11 11:27:25 -0600

    Flesh out packet skeleton
    Create empty fields for the Packet abstract class and implementing classes, as well as trivial getters for those fields as appropriate.

--- src/segmentedfilesystem/Packet.java
+++ src/segmentedfilesystem/Packet.java
@@ -1,10 +1,36 @@
 package segmentedfilesystem;
 public abstract class Packet {
-    protected class HeaderPacket {
-        
+    byte fileID;
+
+    public byte getFileID(){
+        return fileID;
     }
     
-    protected class DataPacket {
-    
+
+
+    protected class HeaderPacket extends Packet{
+        protected String fileName;
+
+        public String getFileName(){
+            return fileName;
+        }
     }
+    
+    protected class DataPacket extends Packet{
+        public boolean isFinal;
+
+        int packetNumber;
+
+        public int getPacketNumber(){
+            return packetNumber;
+        }
+
+        byte[] packetBody;
+
+        public byte[] getPacketBody(){
+            return packetBody;
+        }
+    }
+
+
 }
