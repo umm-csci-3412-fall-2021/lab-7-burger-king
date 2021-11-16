b185e9c2961407294ecdb999230e7a6c677e7aa3
Banana Observed <golds349@morris.umn.edu>
2021-11-16 11:06:16 -0600

    Separate Packet subclasses into new files
    This fixes the perplexing error previously found in the PacketFactory.
    
    Co-authored-by: @day00090 <day00090@morris.umn.edu>

--- src/segmentedfilesystem/Packet.java
+++ src/segmentedfilesystem/Packet.java
@@ -5,32 +5,7 @@ public abstract class Packet {
     public byte getFileID(){
         return fileID;
     }
-    
-
-
-    public class HeaderPacket extends Packet{
-        String fileName;
-
-        public String getFileName(){
-            return fileName;
-        }
-    }
-    
-    public class DataPacket extends Packet{
-        public boolean isFinal;
-
-        int packetNumber;
-
-        public int getPacketNumber(){
-            return packetNumber;
-        }
-
-        byte[] packetBody;
-
-        public byte[] getPacketBody(){
-            return packetBody;
-        }
-    }
-
 
 }
+
+
