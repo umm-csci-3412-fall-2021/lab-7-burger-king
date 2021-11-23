4f367da6ced97b3b2dcf9fc1ea673d624f39478a
Banana Observed <golds349@morris.umn.edu>
2021-11-23 10:19:36 -0600

    Add initial test cases
    Creates some simple test cases for the PacketFactoryClass.  The tests
    are not necessarily correctly formed, but are being commited to be
    better ran on another machine.
    
    Co-authored-by: @day00090 <day00090@morris.umn.edu>

--- test/segmentedfilesystem/PacketFactoryTest.java
+++ test/segmentedfilesystem/PacketFactoryTest.java
@@ -29,13 +29,67 @@ public class PacketFactoryTest {
         assertTrue(testResult instanceof HeaderPacket);
 
         packetByteBuffer[0] = (byte) 1;
+        testResult = factory.buildPacket(testingPacket);
         assertTrue(testResult instanceof DataPacket);
         assertFalse(((DataPacket) testResult).isFinal);
 
         packetByteBuffer[0] = (byte) 3;
+        testResult = factory.buildPacket(testingPacket);
         assertTrue(testResult instanceof DataPacket);
         assertTrue(((DataPacket) testResult).isFinal);
     }
 
+    @Test
+    public void setFileId(){
+        Packet testResult;
+
+        packetByteBuffer[1] = (byte) 3;
+
+        testResult = factory.buildPacket(testingPacket);
+        assertEquals((byte) 3, testResult.getFileID());
+    }
+
+    @Test
+    public void setFileName(){
+        Packet testResult;
+
+        String testName = "Test fileName";
+        byte[] testNameBytes = testName.getBytes();
+        
+        for(int i = 0; i < testNameBytes.length; i++){
+            packetByteBuffer[i + 2] = testNameBytes[i];
+        }
+
+        testResult = factory.buildPacket(testingPacket);
+        assertTrue(testResult instanceof HeaderPacket);
+        assertEquals(testName, ((HeaderPacket) testResult).getFileName());
+
+        String veryLongTestName = "This is an extremely and probably unrealistically long filename which will almost certainly take multiple bytes to store";
+        byte[] veryLongTestBytes = veryLongTestName.getBytes();
+
+        for(int i = 0; i < testNameBytes.length; i++){
+            packetByteBuffer[i + 2] = veryLongTestBytes[i];
+        }
+
+        testResult = factory.buildPacket(testingPacket);
+        assertTrue(testResult instanceof HeaderPacket);
+        assertEquals(veryLongTestName, ((HeaderPacket) testResult).getFileName());
+    }
+
+    @Test
+    public void setFullPacketBody(){
+        Packet testResult;
+
+        packetByteBuffer[0] = (byte) 1;
+        byte[] testBody = new byte[1024];
+        for(int i = 0; i < 1024; i++){
+            testBody[i] = (byte) (i%254);
+            packetByteBuffer[i + 4] = (byte) (i%254);
+        }
+
+        testResult = factory.buildPacket(testingPacket);
+        assertTrue(testResult instanceof DataPacket);
+        assertArrayEquals(testBody, ((DataPacket) testResult).getPacketBody());
+    }
     
 }
